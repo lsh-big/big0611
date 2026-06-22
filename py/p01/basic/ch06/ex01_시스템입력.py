bed04b928ca730cf7e3305a38cd92016ea0feb48
LSH <winho2@naver.com>
2026-06-22 10:11:08 +0900

    06221010

--- py/p01/basic/ch06/ex01_시스템입력.py
+++ py/p01/basic/ch06/ex01_시스템입력.py
@@ -8,4 +8,15 @@ print(input('입력:'))
 '''
 
 name = input('이름을 입력하세요:')
-print(name)
+print(name + '님, 안녕하세요')
+
+# 키에 따른 권장 체중
+# 입력받은 input 값은 문자열이다. -> '170'
+height = int(input('키를 입력하세요:'))
+weight = (height - 100) * 0.9
+# print('당신의 권장체중은 weightkg 입니다.')
+
+print('당신의 권장체중은 ' + str(weight) + 'kg 입니다.')
+
+# print('당신의 권장체중은 {}kg 입니다.'.fomat(weight))
+# print(f'당신의 권장체중은 {weight}kg 입니다.')
