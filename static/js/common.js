01fbdfabac685a7b486e01b0c9b2e59fc5047ad7
LSH <winho2@naver.com>
2026-07-14 17:29:12 +0900

    1729

--- static/js/common.js
+++ static/js/common.js
@@ -32,13 +32,25 @@ document.querySelectorAll(".section").forEach(section=>{
 });
 const topBtn = document.getElementById("topBtn");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        topBtn.classList.add("show");
-    } else {
-        topBtn.classList.remove("show");
-    }
-});
+if (topBtn) {
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            topBtn.classList.add("show");
+        } else {
+            topBtn.classList.remove("show");
+        }
+    });
+
+
+    topBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+}
 
 topBtn.addEventListener("click", () => {
     window.scrollTo({
